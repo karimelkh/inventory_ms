cd839399bf58d8d5931c61127f259b63f305f406
karimelkh <karimelkhanoufi22@gmail.com>
2024-06-29 16:13:38 +0100

    setup main app
    + sqlite3 database

---
+++ main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Product, Category, Location, Supplier
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Supplier)
